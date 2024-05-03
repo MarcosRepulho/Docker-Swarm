901e402e51c735e6eab959dad19d6e766f79b6e4
Marcos Repulho <142548123+MarcosRepulho@users.noreply.github.com>
2024-05-02 23:10:05 -0300

    Alterado Nome, Host Varchar

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
     AlunoID int,
-    Nome varchar(50),
+    Nome varchar(80),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
-    Host varchar(50)
+    Host varchar(80)
 );
